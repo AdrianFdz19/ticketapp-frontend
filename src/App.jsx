21538a3c1ed3819a7fe197c05806f24011e1e64c
AdrianFdz19 <146384358+AdrianrFdez@users.noreply.github.com>
2023-12-09 22:13:29 -0600

    agregar primer parrafo

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ function App() {
   return (
     <div className="App">
       <h3>Pagina principal</h3>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia nihil ratione cumque cum. Molestias vel corporis, impedit nulla, odio asperiores soluta odit voluptatem quibusdam provident animi, distinctio voluptatum quia? Dolorum.</p>
     </div>
   )
 }
