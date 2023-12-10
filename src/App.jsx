4c29c97ae2c04bca297ee7bff9182aab69bcfaf2
AdrianFdz19 <146384358+AdrianrFdez@users.noreply.github.com>
2023-12-09 22:16:21 -0600

    boton agregado

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,10 @@ function App() {
   return (
     <div className="App">
       <h3>Pagina principal</h3>
+      
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia nihil ratione cumque cum. Molestias vel corporis, impedit nulla, odio asperiores soluta odit voluptatem quibusdam provident animi, distinctio voluptatum quia? Dolorum.</p>
+
+      <button>Boton</button>
     </div>
   )
 }
