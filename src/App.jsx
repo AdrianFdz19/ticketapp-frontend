c6e4e20e4a68a5945062852afaade0b65398d2a8
AdrianFdz19 <146384358+AdrianrFdez@users.noreply.github.com>
2023-12-09 22:00:24 -0600

    primer commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="App">
+      <h3>Pagina principal</h3>
+    </div>
+  )
+}
+
+export default App
